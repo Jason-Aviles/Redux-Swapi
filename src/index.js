35c0d2def074f9c2bf6cc3f36169880cca1f72a2
Jason Aviles <sandman2k18@outlook.com>
2019-06-20 01:16:50 -0400

    set up the index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import {composeWithDevTools} from  'redux-devtools-extension'
 // needed dependancies
 // applyMiddleware from redux
+import thunk from  'redux-thunk'
+import logger from 'redux-logger'
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
+
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+ composeWithDevTools(applyMiddleware(thunk,logger))
 );
 
 ReactDOM.render(
